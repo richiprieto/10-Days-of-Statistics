e7ecd3a72e930f071c7f2afe8e887a291b537403
Ricardo M. Prieto <richiprieto@crice.org>
2019-06-05 00:02:03 -0500

    Create d0_weighted_mean.rs

---
+++ d0_weighted_mean.rs
@@ -0,0 +1,33 @@
+use std::io;
+
+fn main(){
+    let mut n = String::new(); 
+    io::stdin().read_line(&mut n).expect("Fallo");
+
+    let mut values = String::new(); 
+    io::stdin().read_line(&mut values).expect("Fallo");
+
+
+    let mut pesos = String::new(); 
+    io::stdin().read_line(&mut pesos).expect("Fallo");
+
+    let values: Vec<f32> = values
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    let pesos: Vec<f32> = pesos
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    let mut nomin: f32 = 0.0;
+    let mut denom: f32 = 0.0;
+
+    for (&v, &w) in values.iter().zip(pesos.iter()){
+        nomin += v * w;
+        denom += w;
+        //println!("{}{}", v, w);
+    }
+    println!("{:.1}", nomin/denom);  
+}
