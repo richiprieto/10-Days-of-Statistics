c112b8ad44d6bad59c7bc5d86d8b6ea31ded9dd8
Ricardo M. Prieto <richiprieto@crice.org>
2019-06-12 20:03:45 -0500

    Create d5_poisson_distribution_2.rs

---
+++ d5_poisson_distribution_2.rs
@@ -0,0 +1,18 @@
+use std::io;
+
+fn main() {
+    let mut vector = String::new();
+    io::stdin().read_line(&mut vector).expect("Error");
+
+    //Convertimos en un vector u32
+    let mut vector: Vec<f32> = vector
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+    .collect();
+
+    let res1 = 160.0 + 40.0 * (vector[0] + vector[0].powf(2.0));
+    let res2 = 128.0 + 40.0 * (vector[1] + vector[1].powf(2.0));
+
+    println!("{:.3}", res1);
+    println!("{:.3}", res2);
+}
